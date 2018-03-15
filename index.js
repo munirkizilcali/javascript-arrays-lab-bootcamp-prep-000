8d4fd6da878170e803dbf22c14079e72db279dcf
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 13:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+  ]
+  
+  function destructivelyAppendKitten(array, item) {
+    array.push(item)
+    return array
+  }
+  
+  
+  
