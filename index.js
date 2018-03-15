6e1065c11d73b43dda59df44198da03deb4df785
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 14:26:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var kittens = [
   }
   
   function removeLastKitten() {
-    var kitty = kittens.slice(0,kittens.lenght()-1)
+    var kitty = kittens.slice(0,kittens.length-1)
     return kitty
   }
   
